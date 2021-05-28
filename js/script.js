a278ecdbee6f8c05c09ea3158eb666c2e211aeb6
Maryna Maksymchuk <risaranma@gmail.com>
2021-05-28 16:13:12 +0300

    Закончила обязательное задание lesson02

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 let money = 600000;
 let income = 'freelance';
-let addExpenses = ['internet', 'books', 'utility bills'];
+let addExpenses = `Books, Utility Bills, Language Lessons`;
 let deposit = Boolean('Я стану крутым JS разработчиком');
 let mission = 1000000;
 let period = 3;
 
-// alert(deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-alert('Прекрасный день для изучения JavaScript!!!');
+console.log(addExpenses.length);
 
-console.log('Сегодня идёт дождь');
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'долларов');
+
+let budgetDay = (money / 30);
+console.log('budgetDay: ', budgetDay);
+
+let array = addExpenses.split(",");
+console.log(array);
+
+
+// alert('Прекрасный день для изучения JavaScript!!!');
+// console.log('Сегодня идёт дождь');
