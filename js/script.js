eddc8d8342165cf53263e2abff50a96d8167202e
Maryna Maksymchuk <risaranma@gmail.com>
2021-05-31 16:20:25 +0300

    change cl

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 
 console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
-console.log('Цель заработать' + ' ' + mission + ' ' + 'долларов');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
 
 let budgetDay = (money / 30);
 console.log('budgetDay: ', budgetDay);
